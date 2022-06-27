a0adf326c997f155b74b529bde53b76e494c1187
Boris Lepikhin <boris@lepikhin.com>
2022-06-26 21:30:37 -0700

    stackable feature compatibility

--- _ide_helpers.php
+++ _ide_helpers.php
@@ -12,6 +12,13 @@ namespace Inertia {
     }
 
     /**
+     * @method \Momentum\Modal\Modal stackable()
+     */
+    class Response
+    {
+    }
+
+    /**
      * @method static \Momentum\Modal\Modal modal(string $component, array $props = [])
      * @method static \Momentum\Modal\Modal dialog(string $component, array $props = [])
      */
--- src/ModalServiceProvider.php
+++ src/ModalServiceProvider.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Momentum\Modal;
 
 use Illuminate\Support\ServiceProvider;
+use Inertia\Response;
 use Inertia\ResponseFactory;
 
 class ModalServiceProvider extends ServiceProvider
@@ -18,11 +19,25 @@ class ModalServiceProvider extends ServiceProvider
             return new Modal($component, $props);
         });
 
+        $this->registerCompatibilityMacros();
+    }
+
+    /**
+     * Extra macros for compatibility with the unreleased official feature
+     * 
+     * @return void 
+     */
+    public function registerCompatibilityMacros(): void
+    {
         ResponseFactory::macro('dialog', function (
             string $component,
             array $props = []
         ) {
             return new Modal($component, $props);
+        });
+
+        Response::macro('stackable', function () {
+            return new Modal($this->component, $this->props);
         });
     }
 }
